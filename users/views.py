53e823e2eb0797e6646def9556b5d5dc27245025
HyungJun Goo <9h0jun1115@gmail.com>
2020-12-22 23:01:34 +0900

    update profile

--- users/views.py
+++ users/views.py
@@ -1,7 +1,7 @@
 import os
 import requests
 from django.contrib.auth import authenticate, login, logout
-from django.views.generic import FormView, DetailView
+from django.views.generic import FormView, DetailView, UpdateView
 from django.urls import reverse_lazy
 from django.shortcuts import redirect, reverse
 from django.core.files.base import ContentFile
@@ -200,3 +200,22 @@ def kakao_callback(request):
 class UserProfileView(DetailView):
     model = models.User
     context_object_name = "user_obj"
+
+
+class UpdateProfileView(UpdateView):
+
+    model = models.User
+    template_name = "users/update-profile.html"
+    fields = (
+        "first_name",
+        "last_name",
+        "avatar",
+        "gender",
+        "bio",
+        "birthdate",
+        "language",
+        "currency",
+    )
+
+    def get_object(self, queryset=None):
+        return self.request.user
