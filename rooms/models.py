db417b4ebc8d1a845e511e6652e0d93569a91dce
HyungJun Goo <9h0jun1115@gmail.com>
2021-01-05 16:11:35 +0900

    Add: Calendar

--- rooms/models.py
+++ rooms/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils import timezone
 from django_countries.fields import CountryField
 from core import models as core_models
 from django.urls import reverse
@@ -122,7 +123,16 @@ class Room(core_models.TimeStampedModel):
         return photos
 
     def get_calendars(self):
-        this_month = Calendar(2021, 1)
-        next_month = Calendar(2021, 2)
+        now = timezone.now()
+        this_year = now.year
+        this_month = now.month
+        next_month = this_month+1
+        if this_month == 12:
+            next_month = 1
         
-        return [this_month, next_month]
+        this_month_cal = Calendar(this_year, this_month)
+        if this_month == 12:
+            this_year += 1
+        next_month_cal = Calendar(this_year, next_month)
+        
+        return [this_month_cal, next_month_cal]
