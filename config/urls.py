9e1b669eb8b4d93fc50017a2046debba5010a9ea
HyungJun Goo <9h0jun1115@gmail.com>
2021-01-31 22:56:37 +0900

    Add: Sentry

--- config/urls.py
+++ config/urls.py
@@ -18,6 +18,8 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
+def trigger_error(request):
+    division_by_zero = 1 / 0
 
 urlpatterns = [
     path("", include("core.urls", namespace="core")),
@@ -28,6 +30,7 @@ urlpatterns = [
     path("reviews/", include("reviews.urls", namespace="reviews")),
     path("lists/", include("lists.urls", namespace="lists")),
     path("admin/", admin.site.urls),
+    path('sentry-debug/', trigger_error),
 ]
 
 if settings.DEBUG:
